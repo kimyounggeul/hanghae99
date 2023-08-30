aa35186a548801dd03c95d24871aded5fad52c9d
kimyounggeul <129300980+kimyounggeul@users.noreply.github.com>
2023-08-30 14:08:03 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 6.85 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,29 @@
+class Solution {
+    // 수박수박수박수박수박수?
+    // 문제 설명
+    // 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+    // 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+    //
+    // input
+    // for문 사용 반복
+    // if문으로 수와 박을 골라냄
+    //
+    // 계산
+    // List String 만들기
+    //
+    // output
+    // int n 에 따른 String n
+
+
+    public String solution(int n) {
+        String answer = "";
+        for (int i=1; i<=n; i++){
+            if (i%2==0){
+                answer += "박";
+            } else {
+                answer += "수";
+            }
+        }
+        return answer;
+    }
+}
